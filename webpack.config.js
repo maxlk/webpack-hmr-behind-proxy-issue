2fae32ddb3d9e5acef7e5ccc7d8665cc1213351f
Maxim Kulikov <maximlkulikov@gmail.com>
2020-02-26 16:37:11 +0200

    explicitly set devServer host, port and publicPath, add CORS headers, add workaround

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,27 @@ module.exports = env => {
     entry: ["./src/index.js"],
     output: {
       filename: "bundle.js",
-      path: path.resolve(__dirname, "dist")
+      path: path.resolve(__dirname, "dist"),
+
+      // [Workaround] uncommenting the following line makes HMR work
+      // publicPath: "http://localhost:8080/webpack-dev-server-root/",
     },
     mode: "development",
     devtool: "cheap-module-eval-source-map",
     watch: true,
     devServer: {
+      host: "localhost",
+      port: 8080,
       hot: true,
       overlay: true,
       sockPort: 8080,
       sockHost: "localhost",
       public: "localhost:8080",
-      disableHostCheck: true
-    }
+      publicPath: "/webpack-dev-server-root/",
+      disableHostCheck: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
   };
 };
